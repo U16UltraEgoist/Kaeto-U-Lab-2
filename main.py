9f095fcdde6c6ea328d7f677b08445f90ad437c3
THEU16EGOIST <kuba29@setoncatholic.org>
2026-09-22 18:15:05 +0000

    GUESSING GAME PART 2

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import random
 
-game_number = random.randint(1,10)
-print(game_number)
+game_number = random.randint(1, 10)
+guess_count = 0
 
-guess = int(input ("Enter a number between 1 and 10: "))
+while True:
+    guess = int(input("Enter a number between 1 and 10: "))
+    guess_count += 1
 
-if guess > game_number:
-    print("Too high")
-elif guess < game_number:
-    print("Too low")
-else:
-    print ("NO DAGESTAN U SAFE")
+    if guess > game_number:
+        print("Too high")
+    elif guess < game_number:
+        print("Too low")
+    else:
+        print(f"You got it in {guess_count} guesses!")
+
+        if guess_count > 5:
+            print("You took more than 5 guesses. Pathetic.")
+        elif guess_count < 5:
+            print("Nice! You got it in less than 5 guesses.")
+            print("Reward: Free pizza and a gold star!")
+        else:
+            print("Exactly 5 guesses. Solid effort.")
+
+        print("NO DAGESTAN U SAFE")
+        break
