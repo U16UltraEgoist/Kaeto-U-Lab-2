da6fb26a9b833427219111659ce8c8e7752a2acb
THEU16EGOIST <kuba29@setoncatholic.org>
2026-09-22 18:19:12 +0000

    PART 3 OF PAIN

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 import random
 
 game_number = random.randint(1, 10)
-guess_count = 0
 
 while True:
-    guess = int(input("Enter a number between 1 and 10: "))
-    guess_count += 1
+    guess_count = 0
 
-    if guess > game_number:
-        print("Too high")
-    elif guess < game_number:
-        print("Too low")
-    else:
-        print(f"You got it in {guess_count} guesses!")
+    while True:
+        guess = int(input("Enter a number between 1 and 10: "))
+        guess_count += 1
 
-        if guess_count > 5:
-            print("You took more than 5 guesses. Pathetic.")
-        elif guess_count < 5:
-            print("Nice! You got it in less than 5 guesses.")
-            print("Reward: Free pizza and a gold star!")
+        if guess > game_number:
+            print("Too high")
+        elif guess < game_number:
+            print("Too low")
         else:
-            print("Exactly 5 guesses. Solid effort.")
+            print(f"You got it in {guess_count} guesses!")
 
-        print("NO DAGESTAN U SAFE")
-        break
+            if guess_count > 5:
+                print("You took more than 5 guesses.")
+            elif guess_count < 5:
+                print("Nice! You got it in less than 5 guesses.")
+                print("Reward: Free pizza and a gold star!")
+            else:
+                print("Exactly 5 guesses. Solid effort.")
+
+            # Generate a new number different from the previous one
+            old_number = game_number
+            while game_number == old_number:
+                game_number = random.randint(1, 10)
+
+            print("A new number has been selected!")
+            break
